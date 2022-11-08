99e3996e26b6ab618f4c2b5c1a0e3808ba9ca96b
Shrijit Singh <shrijitsingh99@gmail.com>
2022-11-08 17:49:51 -0500

    Add depth check

--- scripts/cable_manipulation.py
+++ scripts/cable_manipulation.py
@@ -1,4 +1,5 @@
 import imp
+from random import sample
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -8,9 +9,16 @@ from locate_pixel import locatePixel
 
 
 class CableManipulation:
-    def __init__(self, use_rs=False):
+    def __init__(self, w, h, use_rs=False):
         self.seg = CableSegmentation()
-        if use_rs is True:
+        self.use_rs = use_rs
+        self.image_width = w
+        self.image_height = h
+        self.rim_offset = (
+            100  # for cropping the center area before selecting grasp point
+        )
+        self.vector_grid_dize = 40  # for computing the vector
+        if self.use_rs is True:
             self.realsense = Realsense()
 
     def preprocessImage(self, inputImage, targetColor):
@@ -26,7 +34,9 @@ class CableManipulation:
 
         return mask_oneColor, mask_allOther, result_image, result_image2
 
-    def processImage(self, inputImage, targetColor, visualize=False):
+    def processImage(
+        self, inputImage, targetColor, inputDepth=None, visualize=False
+    ):
         (
             mask_oneColor,
             mask_allOther,
@@ -36,7 +46,7 @@ class CableManipulation:
 
         LP = locatePixel(mask_oneColor, mask_allOther, 55)
         mask_grabOK = LP.iterateImage()
-        res = LP.findVector(mask_grabOK)
+        res = self.findVector(mask_grabOK, inputDepth)
         if res is None:
             RuntimeError("Nothing is found")
         pt, vec = res
@@ -55,6 +65,99 @@ class CableManipulation:
 
         return (mask_grabOK, pt, vec)
 
+    def findVector(self, _mask_grabOK, depth):
+        gridSize = self.vector_grid_dize
+        dh = self.rim_offset
+        dw = self.rim_offset
+        mask_grabOK = _mask_grabOK[
+            dh : self.image_height - dh, dw : self.image_width - dw
+        ]
+        idx = np.argwhere(mask_grabOK > 0)
+
+        # TODO randomly find pixel with valid depth
+        if depth is None:
+            id = np.random.choice(np.arange(idx.shape[0]))
+            r = idx[id, 0]  # row coordinate in the cropped mask
+            c = idx[id, 1]
+            pt = np.array([c + dw, r + dh])
+        else:
+            sample = 0
+            r = None
+            c = None
+            pt = None
+            while sample < 50:
+                id = np.random.choice(np.arange(idx.shape[0]))
+                r = idx[id, 0]  # row coordinate in the cropped mask
+                c = idx[id, 1]
+                pt = np.array(
+                    [c + dw, r + dh]
+                )  # pixel coordinate in the original mask
+                # check if it has valid depth
+                point_c = self.realsense.deproject_pixel(depth, pt[0], pt[1])
+                if point_c[2] >= 0.05 or point_c[2] <= 1:
+                    break
+                sample += 1
+            if r is None:
+                RuntimeError("Sampled 50 pixels, none has valid depth!")
+                if self.use_rs is True:
+                    self.realsense.close()
+                quit(1)
+
+        init_r = min(max(0, r - int((gridSize - 1) / 2)), self.image_height - 1)
+        init_c = min(max(0, c - int((gridSize - 1) / 2)), self.image_width - 1)
+        neighborhood = mask_grabOK[
+            init_r : init_r + gridSize, init_c : init_c + gridSize
+        ]
+        idx_neighborhood = np.argwhere(neighborhood > 0)
+        # if there is more than 1 pixel in this neighbohood and the distance between these neighboods are over a threshold value
+        # out the pixel pair in a list
+        p1 = None
+        p1_idx = 0
+        p2 = None
+        if len(idx_neighborhood) < 2:
+            return None
+        print(len(idx_neighborhood))
+
+        edgeA = np.vstack((np.zeros(gridSize), np.arange(gridSize))).T
+        edgeB = np.vstack(
+            (np.arange(1, gridSize), np.ones(gridSize - 1) * (gridSize - 1))
+        ).T
+        edgeC = np.vstack(
+            (
+                np.ones(gridSize - 1) * (gridSize - 1),
+                np.arange(gridSize - 2, -1, -1),
+            )
+        ).T
+        edgeD = np.vstack(
+            (np.arange(gridSize - 2, 0, -1), np.zeros(gridSize - 2))
+        ).T
+        edges = np.vstack((edgeA, edgeB, edgeC, edgeD))
+        for idx in range(len(edges)):
+            tmp_r = int(edges[idx, 0])
+            tmp_c = int(edges[idx, 1])
+            if neighborhood[tmp_r, tmp_c] > 0:
+                if p1 is None:
+                    p1 = np.array([tmp_r, tmp_c])
+                    p1_idx = idx
+                else:
+                    if idx - p1_idx > gridSize:
+                        p2 = np.array([tmp_r, tmp_c])
+        if p1 is None or p2 is None:
+            return None
+        vec = np.floor(np.flip((p2 - p1)) * 3)
+
+        ptprime = pt + vec
+        if (
+            ptprime[0] < 0
+            or ptprime[0] >= self.image_width
+            or ptprime[1] < 0
+            or ptprime[1] >= self.image_height
+        ):
+            vec = -vec
+        pt = (int(pt[0]), int(pt[1]))
+        vec = (int(vec[0]), int(vec[1]))
+        return pt, vec
+
     def visualize_vector(self, mask_grabOK, pt, ptprime):
         mask_tmp = cv2.cvtColor(
             mask_grabOK.astype(np.uint8), cv2.COLOR_GRAY2BGR
@@ -63,6 +166,7 @@ class CableManipulation:
         # plt.imshow(mask_with_vec)
         cv2.imshow("image", mask_with_vec)
         cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def visualize_processed_images(
         self,
@@ -97,3 +201,4 @@ class CableManipulation:
         ax4.set_axis_off()
         ax5.set_axis_off()
         plt.show()
+        plt.close()
--- scripts/locate_pixel.py
+++ scripts/locate_pixel.py
@@ -23,7 +23,9 @@ class locatePixel:
         # cv2.imshow("img", self.mask_targetColor)
         # cv2.waitKey(0)
         self.mask_restCable = mask_restCable
-        self.boundary_size = boundary_size  # needs to be odd
+        self.boundary_size = (
+            boundary_size  # aka neighborhood size. needs to be odd
+        )
         self.maskH = np.shape(self.mask_targetColor)[0]
         self.maskW = np.shape(self.mask_targetColor)[1]
         self.result = np.zeros((self.maskH, self.maskW))
@@ -74,73 +76,3 @@ class locatePixel:
     def calcDistance(self, x1, y1, x2, y2):
         result = math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
         return result
-
-    def findVector(self, _mask_grabOK):
-        # mask_grabOk = self.iterateImage()
-        dw = 100
-        dh = 100
-        mask_grabOK = _mask_grabOK[dh : self.maskH - dh, dw : self.maskW - dw]
-        idx = np.argwhere(mask_grabOK > 0)
-        # print("number of pixel",len(idx))
-        # quit()
-        gridSize = 40
-
-        # for id in idx:
-        id = np.random.choice(np.arange(idx.shape[0]))
-        r = idx[id, 0]
-        c = idx[id, 1]
-
-        init_r = min(max(0, r - int((gridSize - 1) / 2)), self.maskH - 1)
-        init_c = min(max(0, c - int((gridSize - 1) / 2)), self.maskW - 1)
-        neighborhood = mask_grabOK[
-            init_r : init_r + gridSize, init_c : init_c + gridSize
-        ]
-        idx_neighborhood = np.argwhere(neighborhood > 0)
-        # if there is more than 1 pixel in this neighbohood and the distance between these neighboods are over a threshold value
-        # out the pixel pair in a list
-        p1 = None
-        p1_idx = 0
-        p2 = None
-        if len(idx_neighborhood) < 2:
-            return None
-        print(len(idx_neighborhood))
-
-        edgeA = np.vstack((np.zeros(gridSize), np.arange(gridSize))).T
-        edgeB = np.vstack(
-            (np.arange(1, gridSize), np.ones(gridSize - 1) * (gridSize - 1))
-        ).T
-        edgeC = np.vstack(
-            (
-                np.ones(gridSize - 1) * (gridSize - 1),
-                np.arange(gridSize - 2, -1, -1),
-            )
-        ).T
-        edgeD = np.vstack(
-            (np.arange(gridSize - 2, 0, -1), np.zeros(gridSize - 2))
-        ).T
-        edges = np.vstack((edgeA, edgeB, edgeC, edgeD))
-        for idx in range(len(edges)):
-            tmp_r = int(edges[idx, 0])
-            tmp_c = int(edges[idx, 1])
-            if neighborhood[tmp_r, tmp_c] > 0:
-                if p1 is None:
-                    p1 = np.array([tmp_r, tmp_c])
-                    p1_idx = idx
-                else:
-                    if idx - p1_idx > gridSize:
-                        p2 = np.array([tmp_r, tmp_c])
-        if p1 is None or p2 is None:
-            return None
-        vec = np.floor(np.flip((p2 - p1)) * 3)
-        pt = np.array([c + dw, r + dh])
-        ptprime = pt + vec
-        if (
-            ptprime[0] < 0
-            or ptprime[0] >= self.maskW
-            or ptprime[1] < 0
-            or ptprime[1] >= self.maskH
-        ):
-            vec = -vec
-        pt = (int(pt[0]), int(pt[1]))
-        vec = (int(vec[0]), int(vec[1]))
-        return pt, vec

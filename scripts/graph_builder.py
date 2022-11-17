0befd46d4e795eba49ae49925b413ca1eae1da4b
Te12944265-AMAHA <1603699457@qq.com>
2022-11-17 18:11:04 -0500

    Add compounding graph

--- scripts/graph_builder.py
+++ scripts/graph_builder.py
@@ -37,11 +37,11 @@ class Graph:
     then it is graspable.
     """
 
-    def __init__(self, free_endpoint=None, fixed_endpoint=None, cable=None):
+    def __init__(self, free_endpoint=[], fixed_endpoint=[], cables=[]):
         self.G = nx.Graph(
-            free_endpoint=free_endpoint,
-            fixed_endpoint=fixed_endpoint,
-            cable=cable,
+            free_endpoint=copy.deepcopy(free_endpoint),
+            fixed_endpoint=copy.deepcopy(fixed_endpoint),
+            cables=copy.deepcopy(cables),
         )
 
     def save(self, save_path):
@@ -65,9 +65,7 @@ class Graph:
         return id
 
     def add_node_id(self, id, type, coords=[]):
-        assert self.has_node(id) is False
         self.G.add_node(id, type=type, coords=copy.deepcopy(coords))
-        return id
 
     def has_node(self, id):
         return id in self.G
@@ -104,18 +102,40 @@ class Graph:
     def get_edges(self):
         return list(self.G.edges)
 
+    def set_all_edge_color(self, color):
+        dic = {}
+        for edge in self.get_edges():
+            dic[edge] = color
+        nx.set_edge_attributes(self.G, dic, name="color")
+
+    def get_edge_color(self, id1, id2):
+        assert self.has_edge(id1, id2)
+        return self.G.edges[id1, id2]["color"]
+
     def get_node_coords(self, id):
         """Return a deep copy of the coords list"""
         assert self.has_node(id)
         return copy.deepcopy(self.G.nodes[id]["coords"])
 
-    def set_free_endpoint(self, id):
+    def add_free_endpoint(self, id):
         assert self.has_node(id)
-        self.G.graph["free_endpoint"] = id
+        self.G.graph["free_endpoint"].append(id)
 
-    def set_fixed_endpoint(self, id):
+    def add_fixed_endpoint(self, id):
         assert self.has_node(id)
-        self.G.graph["fixed_endpoint"] = id
+        self.G.graph["fixed_endpoint"].append(id)
+
+    def compose(self, other: "Graph"):
+        res = Graph()
+        res.G = nx.compose(self.G, other.G)
+        res.G.graph["free_endpoint"] = (
+            self.G.graph["free_endpoint"] + other.G.graph["free_endpoint"]
+        )
+        res.G.graph["fixed_endpoint"] = (
+            self.G.graph["fixed_endpoint"] + other.G.graph["fixed_endpoint"]
+        )
+        res.G.graph["cables"] = self.G.graph["cables"] + other.G.graph["cables"]
+        return res
 
     def simplify(self):
         pass
@@ -128,6 +148,9 @@ class Graph:
             if self.is_endpoint(id)
             else "green"
             for id in self.get_nodes()
+        ]
+        e_colors = [
+            self.get_edge_color(edge[0], edge[1]) for edge in self.get_edges()
         ]
         sizes = [
             1000
@@ -154,6 +177,7 @@ class Graph:
             self.G,
             pos=pos,
             node_color=colors,
+            edge_color=e_colors,
             width=widths,
             with_labels=True,
             font_color="white",
@@ -172,7 +196,11 @@ class CableGraph:
         self.compound_graph = None
 
     def create_compound_graph(self):
-        pass
+        graph_prev = None
+        for _, graph in self.graphs.items():
+            if graph_prev is not None:
+                self.compound_graph = graph.compose(graph_prev)
+            graph_prev = graph
 
     def create_graphs(self, cables_data):
         """Create a graph for each cable, where the crossings and the fixed
@@ -181,7 +209,7 @@ class CableGraph:
         Input:
         ``cables_data``: dict of the form {cableID1: data1, cableID2: data2, },
         where data is a dict of the form 
-        {"coords": coords, "pos": pos, "cx": cx}
+        {"coords": coords, "pos": pos, "cx": cx, "color": color}
 
         coords: a N*2 2D list (not np array) of all N inter-connected
         discretization points along the cable, whose first coord is the 
@@ -194,62 +222,99 @@ class CableGraph:
         cx: a M*2 2D list of the coordinates of the M crossings.
         (Note: "coords" might contain repetitive coordinates, but those should
         be listed in cx.)
+
+        color: a string representing the cable color.
         """
         cx_coord_id_map = {}
-        fixed_endpoint_id = None
         for cableID, data in cables_data.items():
             coords = data["coords"]
             pos = data["pos"]
             cx = data["cx"]
-            graph, fixed_endpoint_id = self.create_graph(
-                cableID, coords, pos, cx, cx_coord_id_map, fixed_endpoint_id
+            color = data["color"]
+            graph = self.create_graph(
+                cableID, coords, pos, cx, cx_coord_id_map, color=color
             )
             self.graphs[cableID] = graph
-        print(fixed_endpoint_id)
-        print(cx_coord_id_map)
 
     def create_graph(
-        self, cableID, coords, pos, cx, cx_coord_id_map, fixed_endpoint_id=None
+        self,
+        cableID,
+        coords,
+        pos,
+        cx,
+        cx_coord_id_map,
+        color="black",
+        fixed_endpoint_id=None,
     ):
         assert len(coords) >= 3
-        graph = Graph(cable=cableID)
+        graph = Graph(cables=[cableID])
         pred_id = None
         for i, coord in enumerate(coords):
             if i == 0 or i == len(coords) - 1:
                 id = graph.add_node(NODE_ENDPOINT, coord)
                 if i == 0:
-                    graph.set_free_endpoint(id)
+                    graph.add_free_endpoint(id)
                 else:
                     graph.add_edge(id, pred_id, pos[i - 1])
                     if fixed_endpoint_id is None:
                         fixed_endpoint_id = id
-                    graph.set_fixed_endpoint(fixed_endpoint_id)
+                    graph.add_fixed_endpoint(fixed_endpoint_id)
             else:
                 if coord in cx:
                     id = cx_coord_id_map.get(tuple(coord))
                     if id is None:
                         id = graph.add_node(NODE_CROSSING, coord)
                         cx_coord_id_map[tuple(coord)] = id
+                    else:
+                        graph.add_node_id(id, NODE_CROSSING, coord)
                     graph.add_edge(id, pred_id, pos[i])
                 else:
                     id = graph.add_node(NODE_FREE, coord)
                     graph.add_edge(id, pred_id, pos[i - 1])
             pred_id = id
-        print(cx_coord_id_map)
-        return graph, fixed_endpoint_id
+        graph.set_all_edge_color(color)
+        return graph
 
 
-# test building a graph for a single cable
+# test building a graph for two cables
 cg = CableGraph()
-coords = np.array(
-    [[0, 0], [1, 1], [2, 2], [2, 3], [1, 3], [2, 2], [3, 2], [4, 2]]
+coords1 = np.array(
+    [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+        [3, 3],
+        [3, 4],
+        [2, 5],
+        [1, 4],
+        [1, 3],
+        [2, 2],
+        [3, 2],
+        [4, 2],
+    ]
 )
-cx = [[2, 2]]
-coords = coords.tolist()
-pos = [POS_NONE] * 8
-pos[2] = POS_DOWN
-pos[5] = POS_UP
-data = {"coords": coords, "pos": pos, "cx": cx}
-print(data)
-cg.create_graphs({"test_cable": data})
-cg.graphs["test_cable"].visualize()
+cx1 = [[2, 2], [3, 4], [1, 4]]
+coords1 = coords1.tolist()
+pos1 = [POS_NONE] * 11
+pos1[2] = POS_DOWN
+pos1[4] = POS_DOWN
+pos1[6] = POS_UP
+pos1[8] = POS_UP
+data1 = {"coords": coords1, "pos": pos1, "cx": cx1, "color": "blue"}
+# cables_data = {"cable1": data1}
+# cg.create_graphs(cables_data)
+# cg.graphs["cable1"].visualize()
+
+coords2 = np.array([[0, 6], [0, 5], [1, 4], [2, 3], [3, 4], [4, 5], [5, 5]])
+cx2 = [[3, 4], [1, 4]]
+coords2 = coords2.tolist()
+pos2 = [POS_NONE] * 7
+pos2[2] = POS_DOWN
+pos2[4] = POS_UP
+data2 = {"coords": coords2, "pos": pos2, "cx": cx2, "color": "red"}
+
+
+cables_data = {"cable1": data1, "cable2": data2}
+cg.create_graphs(cables_data)
+cg.create_compound_graph()
+cg.compound_graph.visualize()

9676db4ac415f91b24b4c2859709377c684c1931
Tina Tian <1603699457@qq.com>
2022-11-20 17:02:59 -0500

    .

--- scripts/rs_test.py
+++
@@ -1,9 +0,0 @@
-from rs_driver import Realsense
-import cv2
-import numpy as np
-import open3d as o3d
-
-if __name__ == "__main__":
-    realsense = Realsense()
-    depth, rgb = realsense.getFrameSet()
-    realsense.close()
